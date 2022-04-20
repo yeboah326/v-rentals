9ef247b5cc760d8e34b8851466c0ca9b4e6f5141
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-04-20 08:50:48 +0000

    Add new services for customers

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,16 +1,20 @@
+import React from "react";
 import axios, { AxiosError } from "axios";
 import {
   createUserProps,
   authenticateUserProps,
   AuthContextType,
 } from "../@types/auth";
-import React from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = React.createContext<AuthContextType | null>(null);
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const AuthProvider: React.FC<React.ReactNode> = ({ children }) => {
+  const navigate = useNavigate();
+
   async function createUser({ email, username, password }: createUserProps) {
     try {
       const response = await axios.post(API_URL + "/auth/create", {
@@ -18,6 +22,9 @@ export const AuthProvider: React.FC<React.ReactNode> = ({ children }) => {
         username,
         password,
       });
+      if (response.status === 201) {
+        toast("Account created successfully");
+      }
       return response;
     } catch (error) {
       console.error((error as AxiosError)?.response?.data);
@@ -30,19 +37,25 @@ export const AuthProvider: React.FC<React.ReactNode> = ({ children }) => {
         email,
         password,
       });
-      localStorage.setItem(
-        "user",
-        response.status === 200 ? JSON.stringify(response.data) : ""
-      );
+      if (response.status === 200) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+        navigate("/dashboard");
+        toast("Login successful");
+      }
       console.log(response.data);
-      
     } catch (error) {
       console.error((error as AxiosError)?.response?.data);
     }
   }
 
+  function logoutUser() {
+    localStorage.clear();
+    navigate("/login");
+    toast("Logout successful");
+  }
+
   return (
-    <AuthContext.Provider value={{ createUser, authenticateUser }}>
+    <AuthContext.Provider value={{ createUser, authenticateUser, logoutUser }}>
       {children}
     </AuthContext.Provider>
   );
--- src/services/customers.service.tsx
+++ src/services/customers.service.tsx
@@ -1,6 +1,13 @@
 import axios from "axios";
 import React from "react";
-import { createCustomerProps, CustomerContextType } from "../@types/customers";
+import { toast } from "react-toastify";
+import {
+  createCustomerProps,
+  getCustomerProps,
+  modifyCustomerProps,
+  deleteCustomerProps,
+  CustomerContextType,
+} from "../@types/customers";
 
 export const CustomerContext = React.createContext<CustomerContextType | null>(
   null
@@ -22,17 +29,74 @@ export const CustomerProvider: React.FC<React.ReactNode> = ({ children }) => {
         name,
         telephone,
       });
-      if (response.status === 200) {
-        console.log("User created successfully");
+      if (response.status === 201) {
+        toast("Account created sucessfully");
+        return response;
       }
     } catch (error) {
       console.error(error);
     }
   }
 
+  async function modifyCustomerById({
+    company,
+    email,
+    name,
+    telephone,
+    id,
+  }: modifyCustomerProps) {
+    try {
+      const response = await axios.put(API_URL + `/customer/${id}`, {
+        company,
+        email,
+        name,
+        telephone,
+      });
+      if (response.status === 200) {
+        toast("Customer details updated");
+        return response;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getCustomerById({ id }: getCustomerProps) {
+    try {
+      const response = await axios.get(API_URL + `/customer/${id}`);
+      if (response.status === 200) {
+        return response;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async function getAllCustomers() {
+    try {
+      const response = await axios.get(API_URL + "/customer");
+      if (response.status === 200) {
+        return response;
+      }
+    } catch (error) {}
+  }
+  async function deleteCustomerById({ id }: deleteCustomerProps) {
+    try {
+      const response = await axios.get(API_URL + `/customer/${id}`);
+      if (response.status === 200) {
+        return response;
+      }
+    } catch (error) {}
+  }
+
   return (
     <CustomerContext.Provider
-      value={{ createCustomer }}
+      value={{
+        createCustomer,
+        modifyCustomerById,
+        getCustomerById,
+        getAllCustomers,
+        deleteCustomerById,
+      }}
     ></CustomerContext.Provider>
   );
 };
