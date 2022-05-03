06eeb0e720533f30b78b57f43b830d91ac66089f
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-05-03 13:53:17 +0000

    Add change to customer row

--- src/components/customer_row/customer_row.tsx
+++ src/components/customer_row/customer_row.tsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { CustomText } from "../custom_text/custom_text";
 import { Container } from "./styles";
+
 type CustomerRowType = {
   name: string;
   email: string;
   company: string;
+  telephone: string;
+  id: number;
 };
 
 export const CustomerRow = (customerRow: CustomerRowType) => {
@@ -13,7 +16,15 @@ export const CustomerRow = (customerRow: CustomerRowType) => {
   return (
     <Container
       onClick={() => {
-        navigate("/customers/edit");
+        navigate("/customers/edit", {
+          state: {
+            name: customerRow.name,
+            email: customerRow.email,
+            company: customerRow.company,
+            telephone: customerRow.telephone,
+            id: customerRow.id
+          },
+        });
       }}
     >
       <div style={{ width: "20%" }}>
